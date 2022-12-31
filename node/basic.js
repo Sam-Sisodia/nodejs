524d871779e20918410b80d0e582dd7029706af4
SAJAL <sajalkumar801@gmail.com>
2022-12-31 08:58:37 +0530

    firstnodecommit

---
+++ node/basic.js
@@ -0,0 +1,6 @@
+for(let index=0; index<20 ; index++){
+
+    console.log("hello index :", index)
+}
+
+
---
+++ node/createserver.js
@@ -0,0 +1,25 @@
+// const http  = require("http");
+// const server = http.createServer((req,res) => {
+//     res.write("hello sajal in node js");
+//     res.end();
+// });
+
+// server.listen(3000);
+
+
+
+//other task 
+
+const http  = require("http");
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {"content_type": "application/json"});
+    res.end(
+        JSON.stringify(
+            {
+                data:"hello sajal"
+            }
+        )
+    );
+});
+
+server.listen(3000);
